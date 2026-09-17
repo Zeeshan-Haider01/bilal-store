092cf4969758977d5cdbc25c608cc0991029a8a2
Zeeshan Haider <zeeshan.haider@culturefy.com>
2026-09-17 21:16:58 +0500

    env added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
 import ProductCard from "../Components/ProductCard";
+const url = import.meta.env.VITE_API_KEY_URL;
 
 function Home(){
     let [products , setProducts] =  useState([])
 
     async function getProducts(){
-        let response = await fetch('https://dummyjson.com/products');
+        let response = await fetch(url);
         let data = await response.json(); 
         setProducts(data.products)
     }
